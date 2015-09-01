a7b16707501f628d3305b7c6e7ec38e1b5972294
goofansu <goofan.su@gmail.com>
2015-09-02 03:48:20 +0800

    submit item

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,63 +1,63 @@
 var images = {
-    localId: [],
-    serverId: []
+  localId: [],
+  serverId: []
 };
 
 function doSubmit() {
-    var content = $('#issue_content').val();
-    if(content == "") {
-        alert("请描述一下您遇到的问题，更多的信息有助我们进行排查");
-        return false;
-    } else {
-        var elem = document.getElementById('image_ids');
-        elem.value = images.serverId;
-        images.localId = [];
-        images.serverId = [];
-        return true;
-    }
+  var itemName = document.getElementById('item_name').value;
+  if(itemName == "") {
+    alert("请输入物品名称");
+    return false;
+  } else {
+    var elem = document.getElementById('image_ids');
+    elem.value = images.serverId;
+    images.localId = [];
+    images.serverId = [];
+    return true;
+  }
 }
 
 function appendPreviewImg(localId) {
-    img = document.createElement('img');
-    img.setAttribute('class', 'th');
-    img.src = localId;
-    img.width = 64;
-    img.height = 64;
-    document.getElementById('preview_image').appendChild(img);
+  img = document.createElement('img');
+  img.setAttribute('class', 'th');
+  img.src = localId;
+  img.width = 64;
+  img.height = 64;
+  document.getElementById('preview_image').appendChild(img);
 }
 
 wx.ready(function(){
-    document.getElementById('upload_image').onclick = function () {
-        wx.chooseImage({
+  document.getElementById('upload_image').onclick = function () {
+    wx.chooseImage({
+      success: function (res) {
+        images.localId = res.localIds;
+
+        var i = 0, length = res.localIds.length;
+        function upload() {
+          var localId = res.localIds[i];
+          wx.uploadImage({
+            localId: localId,
             success: function (res) {
-                images.localId = res.localIds;
+              images.serverId.push(res.serverId);
+              appendPreviewImg(localId);
 
-                var i = 0, length = res.localIds.length;
-                function upload() {
-                    var localId = res.localIds[i];
-                    wx.uploadImage({
-                        localId: localId,
-                        success: function (res) {
-                            images.serverId.push(res.serverId);
-                            appendPreviewImg(localId);
-
-                            i++;
-                            if (i < length) {
-                                upload();
-                            }
-                        },
-                        fail: function (res) {
-                            alert(JSON.stringify(res));
-                        }
-                    });
-                }
+              i++;
+              if (i < length) {
                 upload();
+              }
+            },
+            fail: function (res) {
+              alert(JSON.stringify(res));
             }
-        });
-    };
+          });
+        }
+        upload();
+      }
+    });
+  };
 
 });
 
 wx.error(function (res) {
-    alert(res.errMsg);
+  alert(res.errMsg);
 });
