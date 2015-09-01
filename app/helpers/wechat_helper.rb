f76cb10f3122539e3cd0f88b16dbca4a01e11fff
goofansu <goofan.su@gmail.com>
2015-09-01 21:32:55 +0800

    jssdk signature

--- app/helpers/wechat_helper.rb
+++ app/helpers/wechat_helper.rb
@@ -1,2 +1,14 @@
+require 'addressable/uri'
+
 module WechatHelper
+  def self.jssdk_signature(jsapi_ticket, timestamp, nonce, url)
+    uri = Addressable::URI.new
+    uri.query_values = {:timestamp => timestamp,
+                        :noncestr => nonce,
+                        :jsapi_ticket => jsapi_ticket,
+                        :url => url
+                       }.sort
+    str = Addressable::URI.unencode_component(uri.query)
+    Digest::SHA1.hexdigest(str)
+  end
 end
