18cdd20a5c0ab663ebabf549fc9cb62c0101806e
goofansu <goofan.su@gmail.com>
2015-09-03 23:16:19 +0800

    update qiniu

--- config/initializers/qiniu_sdk.rb
+++ config/initializers/qiniu_sdk.rb
@@ -1,2 +1,5 @@
+$qiniu_bucket = Figaro.env.qiniu_bucket
+$qiniu_domain = Figaro.env.qiniu_domain
+
 Qiniu.establish_connection! :access_key => Figaro.env.qiniu_access_key,
                             :secret_key => Figaro.env.qiniu_secret_key
