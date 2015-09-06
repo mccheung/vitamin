27eca55ececc8ac203a0eee18ede28b1b9c0a972
goofansu <goofan.su@gmail.com>
2015-09-06 15:03:53 +0800

    add items index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904185343) do
+ActiveRecord::Schema.define(version: 20150906065758) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150904185343) do
     t.datetime "updated_at",             null: false
   end
 
+  add_index "items", ["name"], name: "index_items_on_name", using: :btree
   add_index "items", ["user_id"], name: "index_items_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
