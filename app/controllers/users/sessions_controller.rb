205f5640c9cdada19e0a5b92a16aba883b06522d
goofansu <goofan.su@gmail.com>
2015-09-06 12:29:04 +0800

    如果在应用内未登录状态，且有openid，则跳转到绑定帐号

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,14 @@
 class Users::SessionsController < Devise::SessionsController
-# before_filter :configure_sign_in_params, only: [:create]
+  # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    if session.has_key?(:openid)
+      redirect_to new_user_registration_path
+    else
+      super
+    end
+  end
 
   # POST /resource/sign_in
   # def create
