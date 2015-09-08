744b7d7a3452cba4fb8822185a20947fc3e8c028
goofansu <goofan.su@gmail.com>
2015-09-08 23:12:00 +0800

    更新

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,7 +8,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     openid = params['oid']
     user = User.find_for_authentication(:openid => openid)
     if user
-      sign_in(:user, user)
+      return sign_in(:user, user)
     else
       if openid
         session[:openid] = openid
