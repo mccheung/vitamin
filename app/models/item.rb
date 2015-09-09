91e0ad32d3c13766f5d15ff7c2448f376f70b8b5
goofansu <goofan.su@gmail.com>
2015-09-09 11:16:20 +0800

    更新添加药品页面

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,7 @@ class Item < ActiveRecord::Base
   belongs_to :user
 
   validates :name, presence: true
+  validates :intro, presence: true
+  validates :buy_from, presence: true
   validates :num, presence: true, numericality: { greater_than: 0 }
 end
