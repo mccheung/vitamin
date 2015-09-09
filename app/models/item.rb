42fb3515c8314eb0e3b51c913b3c1a6e8e72ef13
goofansu <goofan.su@gmail.com>
2015-09-09 16:47:53 +0800

    增加选择日期

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,5 @@ class Item < ActiveRecord::Base
   validates :intro, presence: true
   validates :buy_from, presence: true
   validates :num, presence: true, numericality: { greater_than: 0 }
+  validates_format_of :expire_at, with: /\d{2}\-\d{2}\-\d{2}/
 end
