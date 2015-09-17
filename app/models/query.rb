adb4822f17442d963ad2de45c4afb498d267d8ae
goofansu <goofan.su@gmail.com>
2015-09-17 19:04:17 +0800

    Set default value for query

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,4 +1,10 @@
 class Query
   include ActiveModel::Model
-  attr_accessor :str, :longitude, :latitude
+  attr_accessor :str, :longitude, :latitude, :sort_by
+
+  def initialize(attributes={})
+    super
+    @longitude ||= 121.356
+    @latitude ||= 31.3427
+  end
 end
