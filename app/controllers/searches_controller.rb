f5e1d8e1c8042f506cc2a9732792fb25787fbcb9
goofansu <goofan.su@gmail.com>
2015-10-06 00:16:02 +0800

    Add top queries

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,12 +5,22 @@ class SearchesController < ApplicationController
   def new
     @query = Query.new
     @total = Item.total
+    @top_queries = $namespaced_redis.zrevrange :queries, 0, 9
   end
 
   def index
     @query = Query.new(query_params)
 
     page = params[:page]
+
+    if params['sort_by'].nil? && !@query.str.blank?
+      now = DateTime.now
+      ttl = now.next_week.to_i - now.to_i
+      $namespaced_redis.multi do |multi|
+        multi.zincrby :queries, 1, @query.str
+        multi.expire :queries, ttl
+      end
+    end
 
     if params['sort_by'] == 'num'
       @resp = Item.search_by_num(@query, current_user.id).page(page)
